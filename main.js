fe745cd8e5ec38721751a517f39ab52636df9872
noureddine benkerroum <noureddine.benkerroum@gmail.com>
2024-05-31 14:01:32 +0200

    chore: Update index.html and main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import "./style.css";
+
+
+const start = document.querySelector('.play');
+console.log(start);
+start.addEventListener('click', (e) => {
+
+    e.preventDefault();
+
+    console.log('clicked');
+
+
+})
